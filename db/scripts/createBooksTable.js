25a0e8c11d2529b6ee32886af6ba1ceb72d799ac
mandark1 <amandalwhittaker@yahoo.co.uk>
2022-05-26 16:42:57 +0100

    populated table

--- db/scripts/createBooksTable.js
+++ db/scripts/createBooksTable.js
@@ -1,10 +1,11 @@
 import { pool } from "../index.js";
 
-const sqlString = `CREATE TABLE IF NOT EXISTS books (book_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, author_id INT, title TEXT);`;
-
+//const sqlString = `CREATE TABLE IF NOT EXISTS books (book_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY, author_id INT, title TEXT);`;
+const sqlString = 'SELECT * FROM books;'
 async function createBooksTable() {
     const res = await pool.query(sqlString);
-    console.log("created books table");
+    //console.log("created books table");
+    console.log(res)
 }
 
 createBooksTable();
--- db/scripts/populateBooksTable.js
+++ db/scripts/populateBooksTable.js
@@ -3,10 +3,12 @@ import { books } from "../../libs/data.js";
 
 async function  populateBooksTable() {
     for (let i = 0; i < books.length; i++) {
-        const res = await query(
+        const res = await pool.query(
             `INSERT INTO books (author_id, title) VALUES ($1, $2);`,
             [books[i].author_id, books[i].title]
         );
      console.log(`populated with ${books[i].title}`);  
     }
 }
+
+populateBooksTable();
